24530fc3990991707f6b38b47ca626a49610ab39
zhaogc <zhaogc@fusionskye.com>
2023-06-06 15:08:47 +0800

    generate

--- generate_swft.py
+++ generate_swft.py
@@ -141,38 +141,41 @@ def evaluate(
         **kwargs,
     )
     print(generation_config)
-    # with torch.no_grad():
-    #     print('torch.no_grad execute...')
-    #     if args.use_typewriter:
-    #         for generation_output in model.stream_generate(
-    #             input_ids=input_ids,
-    #             generation_config=generation_config,
-    #             return_dict_in_generate=True,
-    #             output_scores=False,
-    #             repetition_penalty=float(repetition_penalty),
-    #         ):
-    #             outputs = tokenizer.batch_decode(generation_output)
-    #             show_text = "\n--------------------------------------------\n".join(
-    #                 [output.split("### Response:")[1].strip().replace('�','')+" ▌" for output in outputs]
-    #             )
-    #             # if show_text== '':
-    #             #     yield last_show_text
-    #             # else:
-    #             yield show_text
-    #         yield outputs[0].split("### Response:")[1].strip().replace('�','')
-    #     else:
-    #         print('model.generate execute...')
-    #         generation_output = model.generate(
-    #             input_ids=input_ids,
-    #             generation_config=generation_config,
-    #             return_dict_in_generate=True,
-    #             output_scores=False,
-    #             repetition_penalty=1.3,
-    #         )
-    #         output = generation_output.sequences[0]
-    #         output = tokenizer.decode(output).split("### Response:")[1].strip()
-    #         print(output)
-    #         yield output
+    try:
+        with torch.no_grad():
+            print('torch.no_grad execute...')
+            if args.use_typewriter:
+                for generation_output in model.stream_generate(
+                    input_ids=input_ids,
+                    generation_config=generation_config,
+                    return_dict_in_generate=True,
+                    output_scores=False,
+                    repetition_penalty=float(repetition_penalty),
+                ):
+                    outputs = tokenizer.batch_decode(generation_output)
+                    show_text = "\n--------------------------------------------\n".join(
+                        [output.split("### Response:")[1].strip().replace('�','')+" ▌" for output in outputs]
+                    )
+                    # if show_text== '':
+                    #     yield last_show_text
+                    # else:
+                    yield show_text
+                yield outputs[0].split("### Response:")[1].strip().replace('�','')
+            else:
+                print('model.generate execute...')
+                generation_output = model.generate(
+                    input_ids=input_ids,
+                    generation_config=generation_config,
+                    return_dict_in_generate=True,
+                    output_scores=False,
+                    repetition_penalty=1.3,
+                )
+                output = generation_output.sequences[0]
+                output = tokenizer.decode(output).split("### Response:")[1].strip()
+                print(output)
+                yield output
+    except Exception as msg:
+        print(msg)
 
 
 if __name__ == '__main__':
