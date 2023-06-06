05f5b142d4824208e88de355ad18fd26583bc213
zhaogc <zhaogc@fusionskye.com>
2023-06-06 15:13:01 +0800

    generate

--- generate_swft.py
+++ generate_swft.py
@@ -142,38 +142,39 @@ def evaluate(
     )
     print(generation_config)
     try:
-        with torch.no_grad():
-            print('torch.no_grad execute...')
-            if args.use_typewriter:
-                for generation_output in model.stream_generate(
-                    input_ids=input_ids,
-                    generation_config=generation_config,
-                    return_dict_in_generate=True,
-                    output_scores=False,
-                    repetition_penalty=float(repetition_penalty),
-                ):
-                    outputs = tokenizer.batch_decode(generation_output)
-                    show_text = "\n--------------------------------------------\n".join(
-                        [output.split("### Response:")[1].strip().replace('�','')+" ▌" for output in outputs]
+        with torch.autocast("cuda"):
+            with torch.no_grad():
+                print('torch.no_grad execute...')
+                if args.use_typewriter:
+                    for generation_output in model.stream_generate(
+                        input_ids=input_ids,
+                        generation_config=generation_config,
+                        return_dict_in_generate=True,
+                        output_scores=False,
+                        repetition_penalty=float(repetition_penalty),
+                    ):
+                        outputs = tokenizer.batch_decode(generation_output)
+                        show_text = "\n--------------------------------------------\n".join(
+                            [output.split("### Response:")[1].strip().replace('�','')+" ▌" for output in outputs]
+                        )
+                        # if show_text== '':
+                        #     yield last_show_text
+                        # else:
+                        yield show_text
+                    yield outputs[0].split("### Response:")[1].strip().replace('�','')
+                else:
+                    print('model.generate execute...')
+                    generation_output = model.generate(
+                        input_ids=input_ids,
+                        generation_config=generation_config,
+                        return_dict_in_generate=True,
+                        output_scores=False,
+                        repetition_penalty=1.3,
                     )
-                    # if show_text== '':
-                    #     yield last_show_text
-                    # else:
-                    yield show_text
-                yield outputs[0].split("### Response:")[1].strip().replace('�','')
-            else:
-                print('model.generate execute...')
-                generation_output = model.generate(
-                    input_ids=input_ids,
-                    generation_config=generation_config,
-                    return_dict_in_generate=True,
-                    output_scores=False,
-                    repetition_penalty=1.3,
-                )
-                output = generation_output.sequences[0]
-                output = tokenizer.decode(output).split("### Response:")[1].strip()
-                print(output)
-                yield output
+                    output = generation_output.sequences[0]
+                    output = tokenizer.decode(output).split("### Response:")[1].strip()
+                    print(output)
+                    yield output
     except Exception as msg:
         print(msg)
 
