4f3f7d23c6c3a78c1da43f33e9468356e42e264b
zhaogc <zhaogc@fusionskye.com>
2023-06-06 15:20:30 +0800

    generate

--- generate_swft.py
+++ generate_swft.py
@@ -177,7 +177,8 @@ def evaluate(
                     yield output
     except Exception as msg:
         print(msg)
-
+    else:
+        print('无异常')
 
 if __name__ == '__main__':
     input = "###-TASK-A-A-A, no matter feasibility, answer only one word, 'positive' or 'negative', by this sentence:Blockware\u2019s team expects Bitcoin\u2019s adoption rate to be faster than previous technologies, but believes it's still in early-stage growth.\\xa0"
